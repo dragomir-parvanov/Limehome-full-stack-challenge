c34173001d7e716dfb9de73a5d850778c22006cf
Dragomir Parvanov <bieltdrago@gmail.com>
2020-11-21 21:48:40 +0200

    wip: tests initialization

---
+++ build-utils/src/ng-environment.ts
@@ -0,0 +1,9 @@
+/** Manipulating the Angular environment.ts file before building */
+
+export const generateAngularEnvironmentFileContent = (stage: string): string => {
+
+};
+
+export const replaceAngularEnvinronment = (content: string): void => {
+
+};
