3a09eb090ea256778d24667c58d5687bcc1294ba
Dragomir Parvanov <bieltdrago@gmail.com>
2020-11-23 14:31:53 +0200

    wip: fetching map data

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MapService } from './services/map.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private mapService: MapService) {}
+
+  ngOnInit() {
+    this.mapService.init();
+  }
+}
--- src/client/app/services/map.service.ts
+++ src/client/app/services/map.service.ts
@@ -1,11 +1,16 @@
-import { Inject, Injectable } from '@angular/core';
+import { Inject, Injectable, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Subject } from 'rxjs';
+import { BehaviorSubject, Subject, of, Observable } from 'rxjs';
 import { Property } from '../../../types/interfaces/property';
 import {
   DEFAULT_MAP_LATITUDE,
   DEFAULT_MAP_LONGITUDE,
 } from '../../constants/position';
+import { flow, pipe } from 'fp-ts/lib/function';
+import { mergeMap, catchError, map } from 'rxjs/operators';
+import { array } from 'io-ts/lib/Decoder';
+import { PropertyDoceder } from '../../../functions/decoders/properties';
+import { fold } from 'fp-ts/lib/Either';
 
 @Injectable({
   providedIn: 'root',
@@ -15,10 +20,47 @@ export class MapService {
   mapLatitude = DEFAULT_MAP_LATITUDE;
   mapLongitude = DEFAULT_MAP_LONGITUDE;
   properties = new BehaviorSubject<Property[]>([]);
-  constructor(private http: HttpClient) {}
+  private propertiesObservable: Observable<Property[]>;
+  constructor(private http: HttpClient) {
+    const at = `${this.mapLatitude},${this.mapLongitude}`;
 
-  init() {}
+    this.propertiesObservable = this.http.get(`/api/properties/at=${at}`).pipe(
+      catchError((err) => {
+        console.log(
+          `error while retrieving the properties from the server, returning empty array`,
+          err
+        );
+        return of([]);
+      }),
+      map(
+        flow(
+          array(PropertyDoceder).decode,
+          fold(
+            (err) => {
+              console.log(
+                'error when decoding properties, return empty array',
+                err
+              );
+              return [];
+            },
+            (r) => r
+          )
+        )
+      )
+    );
+  }
+
+  init() {
+    this.getAndSetProperties();
+  }
+
   changePropertyFocus(propertyId: string) {
     this.currentFocusedProperty.next(propertyId);
+  }
+
+  private getAndSetProperties() {
+    this.propertiesObservable.subscribe({
+      next: this.properties.next,
+    });
   }
 }
