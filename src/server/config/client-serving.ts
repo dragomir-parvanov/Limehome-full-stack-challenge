1daf0d6565a4beda1268bbc0b3e9dfdb2e8d68d1
Dragomir Parvanov <bieltdrago@gmail.com>
2020-11-24 20:01:51 +0200

    fix: serving

--- src/server/config/client-serving.ts
+++ src/server/config/client-serving.ts
@@ -4,5 +4,9 @@ export const configureClientServing = (app: ReturnType<typeof express>) => {
   const clientPath = path.join(
     '../../../../limehome-full-stack-challenge-client/'
   );
-  app.get('*', express.static(clientPath));
+  app.use('*.*', express.static(clientPath, { maxAge: '1y' }));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(clientPath, 'index.html'));
+  });
 };
